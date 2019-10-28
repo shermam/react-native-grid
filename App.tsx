e42b6cc2e83aeecbe1fe8a534b8f2890c560fe67
Shermam Miranda <shermam.miranda@goodeggs.com>
2019-10-28 16:19:45 -0300

    Add a google login button

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Facebook from 'expo-facebook';
 
-const onPress = () => console.log("facebook")
+const facebookLogin = async () => {
+  const appID = "1378404828993249";
+  const result = await Facebook.logInWithReadPermissionsAsync(appID);
+  console.log(JSON.stringify(result, null, "  "));
+}
+
+const googleLogin = async () => {
+  console.log("google")
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <Button title="Facebook" onPress={onPress} />
+      <Button title="Facebook" onPress={facebookLogin} />
+      <Button title="Google" onPress={googleLogin} />
     </View>
   );
 }
