831643bd12f957983ed85cf52d42e82483a86ac5
Shermam Miranda <shermam.miranda@goodeggs.com>
2019-10-28 14:27:07 -0300

    Add facebook button

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const onPress = () => console.log("facebook")
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="Facebook" onPress={onPress} />
     </View>
   );
 }
